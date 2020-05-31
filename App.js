cbebec3a0217ed3fceb314bbe45c1a5582d265c6
MarlonBeloMarques <marlon.belohd@gmail.com>
2020-05-31 15:57:12 -0400

    inital grid screen completed

--- App.js
+++ App.js
@@ -1,23 +1,58 @@
-import React from 'react';
-import { StyleSheet, Text, View, Image, ScrollView } from "react-native";
+import React, { useEffect, useState } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  FlatList,
+  Dimensions,
+} from "react-native";
 import PHOTOS from "./src/data";
+import { processImages, buildRows, normalizeRows } from "./src/utils";
+
+const maxWidth = Dimensions.get("window").width;
+
 
 function Item ({ item }) {
   return (
-    <Image 
-      source={{ uri: item.url }} 
-      style={{ width: 200, height: 200 }} 
+    <Image
+      source={{ uri: item.url }}
+      style={{
+        width: item.width,
+        height: item.height
+      }}
     />
   )
 } 
 
 export default function App() {
+
+  const [dataSource, setDataSource] = useState()
+
+  useEffect(() => {
+    const processedImages = processImages(PHOTOS)
+    let rows = buildRows(processedImages, maxWidth)
+    rows = normalizeRows(rows, maxWidth)
+
+    setDataSource(rows)
+
+  }, [])
+
+  function renderItem({ item, index }) {
+    return(
+      <View
+        style={{
+          flexDirection: 'row',
+          marginBottom: 5,
+          justifyContent: 'space-between'
+        }}>
+          {item.map(item => <Item item={item} key={item.id} />)}
+      </View>
+    )
+  }
+
   return (
-    <ScrollView style={styles.container}>
-      {PHOTOS.map((photo) => (
-        <Item key={photo.id} item={photo} />
-      ))}
-    </ScrollView>
+    <FlatList data={dataSource} renderItem={renderItem} />
   );
 }
 
