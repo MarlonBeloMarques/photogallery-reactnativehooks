2c810dc30ad27d5479c398a4d7989790f619ee42
MarlonBeloMarques <marlon.belohd@gmail.com>
2020-05-30 22:35:03 -0400

    added scrollview and rendered the data

--- App.js
+++ App.js
@@ -1,19 +1,29 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, ScrollView } from "react-native";
+import PHOTOS from "./src/data";
+
+function Item ({ item }) {
+  return (
+    <Image 
+      source={{ uri: item.url }} 
+      style={{ width: 200, height: 200 }} 
+    />
+  )
+} 
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <ScrollView style={styles.container}>
+      {PHOTOS.map((photo) => (
+        <Item key={photo.id} item={photo} />
+      ))}
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff"
   },
 });
